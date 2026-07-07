01a2fbb2d9321c9e104024d97240efc6f36972f4
qida <sunqida@foxmail.com>
2026-07-07 18:41:46 +0800

    fix(gin): 优化跨域中间件配置
    调整跨域允许的头信息和请求方法，简化origin判断逻辑，直接使用通配符允许所有来源

--- httpx/gin.go
+++ httpx/gin.go
@@ -94,15 +94,13 @@ func RecoverGin(c *gin.Context) {
 
 func CorsGin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		c.Header("Access-Control-Allow-Origin", origin)
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		// 放行所有OPTIONS方法
-		if method == "OPTIONS" {
+		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
 		}
 		// 处理请求
